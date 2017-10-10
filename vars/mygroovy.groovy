1ccf2c6721ccef5e4289cd8c6d8938de27bec924
sushil pandey <pandey.sushil64@gmail.com>
2017-10-10 22:30:52 +0530

    Update mygroovy.groovy

--- vars/mygroovy.groovy
+++ vars/mygroovy.groovy
@@ -1,11 +1,14 @@
 #!/usr/bin/env groovy
 
+import POC
 /**
  * Send notifications based on build status string
  */
 def call(String buildStatus = 'STARTED'){
  println('shared libary called')
  FunctionExample()
+ def poc = POC()
+ println poc.getX()
 }
 
 def FunctionExample(){
