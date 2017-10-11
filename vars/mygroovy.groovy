0df081df72231d25a51d0709f2319e9061f2b1a4
sushil pandey <pandey.sushil64@gmail.com>
2017-10-11 09:38:49 +0530

    Update mygroovy.groovy

--- vars/mygroovy.groovy
+++ vars/mygroovy.groovy
@@ -38,7 +38,34 @@ def FunctionExample(){
 }
 */
 
-import GroupInfo
+//import GroupInfo
+import java.io.*;
+            class Group implements java.io.Serializable {
+            String groupName
+            List<String> InstallSteps = []
+            List<Pipeline> pipelines = []
+            void addPipeline( Pipeline p ) {
+                pipelines << p
+            }
+            Group leftShift( Pipeline p ) {
+                addPipeline( p )
+                this
+            }
+            String toString() {
+                "$groupName, $InstallSteps $pipelines"
+            }
+        }
+        
+     class Pipeline implements java.io.Serializable{
+        String pipelineName
+        String inProgress
+        List<String>ipList = []
+        List<String> testCmd = []
+        List<String> testParamList = []
+        String toString() {
+            "($pipelineName $isProgress $testCmd $testParam)"
+        }
+    }
 
 def call(String stage = 'INSTALL'){
  println('shared libary called')
