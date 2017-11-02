d78c8ddd7ca39f4b6d815f4b972ae2807bd57552
sushil pandey <pandey.sushil64@gmail.com>
2017-11-02 09:48:48 +0530

    Update mygroovy.groovy

--- vars/mygroovy.groovy
+++ vars/mygroovy.groovy
@@ -1,43 +1,5 @@
 #!/usr/bin/env groovy
 
-//import POC
-/**
- * Send notifications based on build status string
- */
-/*
-import java.io.*;
-class POC implements java.io.Serializable{
-int x, y, z;
-POC(){
-x = 2;
-y = 3;
-z = 4;
-}
-int getX(){
-return x;
-}
-int getY(){
-return y;
-}
-int getz(){
-return z;
-}
-}
-
-
-def call(String buildStatus = 'STARTED'){
- println('shared libary called')
- FunctionExample()
- return "Functionality Complete"
-}
-
-def FunctionExample(){
-  def poc = new POC()
-  println poc.getX()
-  println('Invoke different function')
-}
-*/
-
 //import GroupInfo
 import java.io.*;
             class Group implements java.io.Serializable {
